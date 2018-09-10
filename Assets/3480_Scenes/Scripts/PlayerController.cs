3037d1c5d1cefdadfb6b8fd4b298e6b256210f28
Rachit Goyal <rachit.goyal@knights.ucf.edu>
2018-09-10 17:05:04 -0400

    Added Script
    Just added the remaining script

--- Assets/3480_Scenes/Scripts/PlayerController.cs
+++ Assets/3480_Scenes/Scripts/PlayerController.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
-    private int csount; 
+    private int count; 
 
 	void Start ()
 	{
         rb = GetComponent<Rigidbody>();
         count = 0;
+        SetCountText ();
+        winText.text = "";
 	}
 
 	void FixedUpdate ()
@@ -30,6 +35,16 @@ public class PlayerController : MonoBehaviour {
         {
             other.gameObject.SetActive(false);
             count = count + 1;
+            SetCountText ();
         }    
-	}
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();  
+        if (count >= 14)
+        {
+            winText.text = "You Win";
+        }    
+    }
 }
